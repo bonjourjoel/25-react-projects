edc9cd1c19b09315cc9aedc59b091410d6ad2a83
joel <bonjourjoel@gmail.com>
2024-02-14 13:53:12 +0100

    fix mangling of components names

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,23 @@
-import { Navigate, Route, Routes, useLocation, useNavigate } from 'react-router-dom'
-import './App.css'
-import React, { useState } from 'react';
-import { APP_COMPONENTS } from './AppComponents';
+import {
+  Navigate,
+  Route,
+  Routes,
+  useLocation,
+  useNavigate,
+} from "react-router-dom";
+import "./App.css";
+import React, { useState } from "react";
+import { APP_COMPONENTS } from "./AppComponents";
 
-const APPS : Array<{ componentName: string, path: string, component: () => React.JSX.Element }> =
-  APP_COMPONENTS.map(component => ({
-    componentName: component.prototype.constructor.name,
-    path: "/" + component.prototype.constructor.name.toLowerCase(),
-    component: component,
-  }));
+const APPS: Array<{
+  componentName: string;
+  path: string;
+  component: () => React.JSX.Element;
+}> = APP_COMPONENTS.map(([componentName, component]) => ({
+  componentName: componentName,
+  path: "/" + component.prototype.constructor.name.toLowerCase(),
+  component: component,
+}));
 
 function App() {
   const navigate = useNavigate();
@@ -17,41 +26,48 @@ function App() {
 
   return (
     <>
-        <div>
-          <big>Joel's ReactJs tutorial <a href="https://www.youtube.com/watch?v=5ZdHfJVAY-s" target="_blank">25 react apps</a> :</big>
-          &nbsp;
-          For each app, I try to program it myself, and after I play the tutorial's solution and copy it. Then I compare the two versions, and finally I may improve mine.
-          Now please select an app:
-          &nbsp;
-          <select
-            value={appPath}
-            onChange={e => {
-              const newAppPath : string = e.target.value;
-              setAppPath(newAppPath);
-              navigate(newAppPath);
-            }}
-          >
-            {
-              APPS.map((app) => (
-                <option key={app.path} value={app.path}>{app.componentName}</option>
-              ))
-            }
-          </select>
-          &nbsp;
-          <button onClick={() => document.location.reload()}>Reload</button>
-        </div>
+      <div>
+        <big>
+          Joel's ReactJs tutorial{" "}
+          <a href="https://www.youtube.com/watch?v=5ZdHfJVAY-s" target="_blank">
+            25 react apps
+          </a>{" "}
+          :
+        </big>
+        &nbsp; For each app, I try to program it myself, and after I play the
+        tutorial's solution and copy it. Then I compare the two versions, and
+        finally I may improve mine. Now please select an app: &nbsp;
+        <select
+          value={appPath}
+          onChange={(e) => {
+            const newAppPath: string = e.target.value;
+            setAppPath(newAppPath);
+            navigate(newAppPath);
+          }}
+        >
+          {APPS.map((app) => (
+            <option key={app.path} value={app.path}>
+              {app.componentName}
+            </option>
+          ))}
+        </select>
+        &nbsp;
+        <button onClick={() => document.location.reload()}>Reload</button>
+      </div>
       <hr />
       <Routes>
         <Route path="/" element={<Navigate replace to={APPS[0].path} />} />
-        {
-            APPS.map(app => (
-              <Route key={app.path} path={app.path} element={app.component()}></Route>
-            ))
-        }
+        {APPS.map((app) => (
+          <Route
+            key={app.path}
+            path={app.path}
+            element={app.component()}
+          ></Route>
+        ))}
         <Route path="*" element={<div>Error 404 Not found</div>} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
